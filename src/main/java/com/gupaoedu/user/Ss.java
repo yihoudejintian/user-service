605e70bf3d82b365d01bd7be47ca7c5c65f8e0cf
tongqinyuan <yuantongqin@ccmtjf.com>
2018-07-20 16:58:12 +0800

    第一提交代码1.1

---
+++ src/main/java/com/gupaoedu/user/Ss.java
@@ -0,0 +1,12 @@
+package com.gupaoedu.user;
+
+/**
+ * @author yuantongqin
+ * @date 2018/7/20 下午4:57
+ */
+public class Ss {
+
+    public static void main(String[] args) {
+        System.out.println("没什么好说的");
+    }
+}
